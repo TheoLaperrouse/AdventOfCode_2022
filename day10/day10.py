e4415a399a11467fecafd880bb93809564bac185
Théo Laperrouse <theo.laperrouse@laposte.net>
2022-12-10 19:17:58 +0100

    Day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,64 +1,55 @@
 import pathlib
 import sys
 
-
-def move_dir(position):  # , move):
-    # match move:
-    #     case 'R':
-    #         position[0] += 1
-    #     case 'L':
-    #         position[0] -= 1
-    #     case 'U':
-    #         position[1] += 1
-    #     case 'D':
-    #         position[1] -= 1
-    return position
-
-
 def parse(text_input):
     """Parse input."""
-    return [[line.split()[0], int(line.split()[1])] for line in text_input.split('\n')]
+    return list(text_input.split('\n'))
 
 
-def part1(moves_inputs):
+def part1(cycle_inputs):
     """Solve part 1."""
-    visited_positions = []
-    last_position_H = [0, 0]
-    position_H = [0, 0]
-    position_T = [-1, 0]
-    last_dist = 0
-    for move in moves_inputs:
-        for _ in range(0, move[1]):
-            position_H = move_dir(position_H, move[0])
-            dist = abs(position_H[0] - position_T[0]) + \
-                abs(position_H[1] - position_T[1])
-            if last_dist > dist:
-                pass
-            # Diagonale
-            elif abs(position_H[0] - position_T[0]) + abs(position_H[1] - position_T[1]) == 2\
-                    and abs(position_H[0] - position_T[0]) > 0 and abs(position_H[1] - position_T[1]) > 0:
-                position_T = last_position_H[:]
-                visited_positions.append(position_T)
-            # H revient vers T (T bouge pas)
-
-            # Comportement normal
-            else:
-                position_T = move_dir(position_T, move[0])
-                visited_positions.append(position_T[:])
-            last_position_H = position_H[:]
-            print(position_H, position_T)
-            last_dist = dist
-    known_positions = []
-    for position in visited_positions:
-        if position not in known_positions:
-            known_positions.append(position)
-    print(known_positions)
-    return len(known_positions)
+    res = 1
+    cycles = [0]
+    somme = 0
+    for cycle in cycle_inputs:
+        if cycle == 'noop':
+            cycles.append(res)
+        elif 'addx' in cycle :
+            val = int(cycle.split()[1])
+            cycles.append(res)
+            res += val
+            cycles.append(res)
+    important_cycles = [20, 60, 100, 140, 180, 220]
+    for i in important_cycles :
+        somme += cycles[i - 1] * i
+    return somme
 
 
-def part2(moves_inputs):
+def part2(cycle_inputs):
     """Solve part 2."""
-    return 0
+    res = 1
+    cycles = [0]
+    for cycle in cycle_inputs:
+        if cycle == 'noop':
+            cycles.append(res)
+        elif 'addx' in cycle :
+            val = int(cycle.split()[1])
+            cycles.append(res)
+            res += val
+            cycles.append(res)
+    string = ''
+    for index, i in enumerate(cycles):
+        actual_index = index % 40
+        print(actual_index)
+        if i in [actual_index - 1, actual_index, actual_index + 1]:
+            cycles[index] = '#'
+        else :
+            cycles[index] = ' '
+    for index, i in enumerate(cycles):
+        if index % 40 == 0:
+            string += '\n'
+        string += i
+    return string
 
 
 if __name__ == "__main__":
