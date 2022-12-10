c017d3e430f4e4bebb71fedbe62347d560392201
Théo Laperrouse <theo.laperrouse@energiency.com>
2022-12-10 16:07:55 +0100

    commit message

--- day10/day10.py
+++ day10/day10.py
@@ -2,16 +2,16 @@ import pathlib
 import sys
 
 
-def move_dir(position, move):
-    match move:
-        case 'R':
-            position[0] += 1
-        case 'L':
-            position[0] -= 1
-        case 'U':
-            position[1] += 1
-        case 'D':
-            position[1] -= 1
+def move_dir(position):  # , move):
+    # match move:
+    #     case 'R':
+    #         position[0] += 1
+    #     case 'L':
+    #         position[0] -= 1
+    #     case 'U':
+    #         position[1] += 1
+    #     case 'D':
+    #         position[1] -= 1
     return position
 
 
@@ -32,24 +32,27 @@ def part1(moves_inputs):
             position_H = move_dir(position_H, move[0])
             dist = abs(position_H[0] - position_T[0]) + \
                 abs(position_H[1] - position_T[1])
+            if last_dist > dist:
+                pass
             # Diagonale
-            if abs(position_H[0] - position_T[0]) + abs(position_H[1] - position_T[1]) == 2\
+            elif abs(position_H[0] - position_T[0]) + abs(position_H[1] - position_T[1]) == 2\
                     and abs(position_H[0] - position_T[0]) > 0 and abs(position_H[1] - position_T[1]) > 0:
                 position_T = last_position_H[:]
                 visited_positions.append(position_T)
             # H revient vers T (T bouge pas)
-            elif last_dist > dist:
-                pass
+
             # Comportement normal
             else:
                 position_T = move_dir(position_T, move[0])
                 visited_positions.append(position_T[:])
             last_position_H = position_H[:]
+            print(position_H, position_T)
             last_dist = dist
     known_positions = []
     for position in visited_positions:
         if position not in known_positions:
             known_positions.append(position)
+    print(known_positions)
     return len(known_positions)
 
 
